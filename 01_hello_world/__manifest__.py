64ee8e96d72d65901a94769807063b11a8c16b66
OmarCrespoCarrazana <ing.omarcc92@gmail.com>
2023-10-21 16:53:21 -0500

    First example done
    Created all the basic folders and files of a module.

---
+++ 01_hello_world/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': '01 Hello World',
+    'version': '16.0.1.0',
+    'author': 'Omar Crespo Carrazana',
+    'maintainer': 'Omar Crespo Carrazana',
+    'summary': 'First Example',
+    'description': 'Hello world pets first exercise',
+    'category': 'Extra Tools',
+    'depends': ['base'],
+    'data': ['views/view.xml',
+             'security/ir.model.access.csv'],
+    'installable': True,
+    'application': True,
+}
---
+++ 01_hello_world/models/ej_pet.py
@@ -0,0 +1,15 @@
+from odoo import api, models, fields
+
+
+class EjPet(models.Model):
+    _name = 'ej.pet'
+    name = fields.Char('Name', required=True)
+    age = fields.Float('Age in Years', required=True)
+    color = fields.Char('Color')
+    species = fields.Selection(selection=[('dog', "Dog"),
+                                          ('cat', "Cat"),
+                                          ('bird', "Bird"),
+                                          ('other', "Other")], required=True, default='dog')
+    size = fields.Selection([('small', 'Small'),
+                             ('medium', 'Medium'),
+                             ('big', 'Big')], 'Size', required=True)
