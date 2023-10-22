172bb9c5fab5f6898474fb52ae95ad34493da261
OmarCrespoCarrazana <ing.omarcc92@gmail.com>
2023-10-22 10:38:53 -0500

    data added
    the data of the 5th example is added in the 3rd module

--- 03_calculated_fields/__manifest__.py
+++ 03_calculated_fields/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': '03 Calculated Fields',
-    'version': '16.0.1.0',
+    'version': '16.0.1.1',
     'author': 'Omar Crespo Carrazana',
     'maintainer': 'Omar Crespo Carrazana',
     'summary': 'Hello World will be extended',
     'description': 'Hello world pets third exercise',
     'category': 'Extra Tools',
     'depends': ['01_hello_world'],
-    'data': ['views/pet_views.xml'],
+    'data': ['views/pet_views.xml',
+             'data/pet_data.xml'],
     'installable': True,
 }
