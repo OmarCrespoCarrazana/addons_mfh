f039b8073918958118fcc2718563f6029fc4d022
OmarCrespoCarrazana <ing.omarcc92@gmail.com>
2023-10-22 20:11:34 -0500

    Start 12 video
    Wizard started

---
+++ 12_wizard_upload_ftp/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': '12 wizard upload ftp',
+    'version': '16.0.1.0',
+    'author': 'Omar Crespo Carrazana',
+    'maintainer': 'Omar Crespo Carrazana',
+    'category': 'Extra Tools',
+    'depends': ['base'],
+    'data': []
+}
